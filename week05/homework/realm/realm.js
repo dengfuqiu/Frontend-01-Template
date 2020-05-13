cf176942eb4b3fc381da1c5264c15eeb1bba706f
Hitomi <fuqiu.deng@socialcredits.cn>
2020-05-13 15:53:54 +0800

    feat: 第五周作业基本上完成

---
+++ week05/homework/realm/realm.js
@@ -0,0 +1,103 @@
+const set = new Set();
+const globalProperties = [
+  'Infinity',
+  'NaN',
+  'undefined',
+  'eval',
+  'isFinite',
+  'isNaN',
+  'parseFloat',
+  'parseInt',
+  'decodeURI',
+  'decodeURIComponent',
+  'encodeURI',
+  'encodeURIComponent',
+  'Array',
+  'Date',
+  'RegExp',
+  'Promise',
+  'Proxy',
+  'Map',
+  'WeakMap',
+  'Set',
+  'WeakSet',
+  'Function',
+  'Boolean',
+  'String',
+  'Number',
+  'Symbol',
+  'Object',
+  'Error',
+  'EvalError',
+  'RangeError',
+  'ReferenceError',
+  'SyntaxError',
+  'TypeError',
+  'URIError',
+  'ArrayBuffer',
+  'SharedArrayBuffer',
+  'DataView',
+  'Float32Array',
+  'Float64Array',
+  'Int8Array',
+  'Int16Array',
+  'Int32Array',
+  'Uint8Array',
+  'Uint16Array',
+  'Uint32Array',
+  'Uint8ClampedArray',
+  'Atomics',
+  'JSON',
+  'Math',
+  'Reflect',
+  'escape',
+  'unescape',
+]
+
+const queue = [];
+
+for (const p of globalProperties) {
+  queue.push({
+    path: [p],
+    object: this[p]
+  });
+}
+
+let current;
+
+while (queue.length) {
+  current = queue.shift();
+  console.log(current.path.join('.'));
+  if (set.has(current.object) || current.object === undefined) {
+    continue;
+  }
+  set.add(current.object);
+
+  for (let p of Object.getOwnPropertyNames(current.object)) {
+    const property = Object.getOwnPropertyDescriptor(current.object, p);
+    if (
+      property.hasOwnProperty('value')
+      && (
+        (property.value != null) && (typeof property.value === 'object') || (typeof property.value === 'object')
+      )
+      && property.value instanceof Object
+    ) {
+      queue.push({
+        path: current.path.concat([p]),
+        object: property.value
+      });
+    }
+    if (property.hasOwnProperty('get') && (typeof property.get === 'function') ) {
+      queue.push({
+        path: current.path.concat([p]),
+        object: property.get
+      });
+    }
+    if (property.hasOwnProperty('set') && (typeof property.set === 'function') ) {
+      queue.push({
+        path: current.path.concat([p]),
+        object: property.set
+      });
+    }
+  }
+}
